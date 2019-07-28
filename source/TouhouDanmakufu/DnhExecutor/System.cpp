3495ec1ef0bc4dd497249058ab08d37c99ec47b0
CreepyNinja_ <creepyninja.yt@gmail.com>
2019-07-28 15:17:07 +0200

    clang-format: lint System

--- source/TouhouDanmakufu/DnhExecutor/System.cpp
+++ source/TouhouDanmakufu/DnhExecutor/System.cpp
@@ -1,7 +1,7 @@
-#include"System.hpp"
-#include"TitleScene.hpp"
-#include"ScriptSelectScene.hpp"
-#include"StgScene.hpp"
+#include "System.hpp"
+#include "ScriptSelectScene.hpp"
+#include "StgScene.hpp"
+#include "TitleScene.hpp"
 
 /**********************************************************
 //SystemController
@@ -16,8 +16,8 @@ SystemController::SystemController()
 	ETaskManager* taskManager = ETaskManager::GetInstance();
 	ref_count_ptr<SystemResidentTask> task = new SystemResidentTask();
 	taskManager->AddTask(task);
-	taskManager->AddRenderFunction(TTaskFunction<SystemResidentTask>::Create(
-		task, &SystemResidentTask::RenderFps), SystemResidentTask::TASK_PRI_RENDER_FPS);
+	taskManager->AddRenderFunction(TTaskFunction<SystemResidentTask>::Create(task, &SystemResidentTask::RenderFps),
+		SystemResidentTask::TASK_PRI_RENDER_FPS);
 }
 SystemController::~SystemController()
 {
@@ -31,15 +31,12 @@ void SystemController::Reset()
 
 	DnhConfiguration* config = DnhConfiguration::CreateInstance();
 	std::wstring pathPackageScript = config->GetPackageScriptPath();
-	if(pathPackageScript.size() == 0)
-	{
+	if (pathPackageScript.size() == 0) {
 		infoSystem_->UpdateFreePlayerScriptInformationList();
 		sceneManager_->TransTitleScene();
-	}
-	else
-	{
+	} else {
 		ref_count_ptr<ScriptInformation> info = ScriptInformation::CreateScriptInformation(pathPackageScript, false);
-		if(info == NULL)
+		if (info == NULL)
 			ShowErrorDialog(ErrorUtility::GetFileNotFoundErrorMessage(pathPackageScript));
 		sceneManager_->TransPackageScene(info, true);
 	}
@@ -81,10 +78,10 @@ void SceneManager::TransTitleScene()
 	ETaskManager* taskManager = ETaskManager::GetInstance();
 	ref_count_ptr<TitleScene> task = new TitleScene();
 	taskManager->AddTask(task);
-	taskManager->AddWorkFunction(TTaskFunction<TitleScene>::Create(
-		task, &TitleScene::Work), TitleScene::TASK_PRI_WORK);
-	taskManager->AddRenderFunction(TTaskFunction<TitleScene>::Create(
-		task, &TitleScene::Render), TitleScene::TASK_PRI_RENDER);
+	taskManager->AddWorkFunction(TTaskFunction<TitleScene>::Create(task, &TitleScene::Work),
+		TitleScene::TASK_PRI_WORK);
+	taskManager->AddRenderFunction(TTaskFunction<TitleScene>::Create(task, &TitleScene::Render),
+		TitleScene::TASK_PRI_RENDER);
 }
 void SceneManager::TransScriptSelectScene(int type)
 {
@@ -98,25 +95,22 @@ void SceneManager::TransScriptSelectScene(int type)
 	ETaskManager* taskManager = ETaskManager::GetInstance();
 	ref_count_ptr<ScriptSelectScene> task = new ScriptSelectScene();
 	taskManager->AddTask(task);
-	taskManager->AddWorkFunction(TTaskFunction<ScriptSelectScene>::Create(
-		task, &ScriptSelectScene::Work), ScriptSelectScene::TASK_PRI_WORK);
-	taskManager->AddRenderFunction(TTaskFunction<ScriptSelectScene>::Create(
-		task, &ScriptSelectScene::Render), ScriptSelectScene::TASK_PRI_RENDER);
+	taskManager->AddWorkFunction(TTaskFunction<ScriptSelectScene>::Create(task, &ScriptSelectScene::Work),
+		ScriptSelectScene::TASK_PRI_WORK);
+	taskManager->AddRenderFunction(TTaskFunction<ScriptSelectScene>::Create(task, &ScriptSelectScene::Render),
+		ScriptSelectScene::TASK_PRI_RENDER);
 
 	ref_count_ptr<ScriptSelectModel> model = NULL;
-	if(type == ScriptSelectScene::TYPE_SINGLE || 
-		type == ScriptSelectScene::TYPE_PLURAL || 
-		type == ScriptSelectScene::TYPE_STAGE || 
-		type == ScriptSelectScene::TYPE_PACKAGE || 
-		type == ScriptSelectScene::TYPE_DIR || 
-		type == ScriptSelectScene::TYPE_ALL )
-	{
+	if (type == ScriptSelectScene::TYPE_SINGLE
+		|| type == ScriptSelectScene::TYPE_PLURAL
+		|| type == ScriptSelectScene::TYPE_STAGE
+		|| type == ScriptSelectScene::TYPE_PACKAGE
+		|| type == ScriptSelectScene::TYPE_DIR
+		|| type == ScriptSelectScene::TYPE_ALL) {
 		std::wstring dir = EPathProperty::GetStgScriptRootDirectory();
 		SystemInformation* systemInfo = SystemController::GetInstance()->GetSystemInformation();
-		if(type == ScriptSelectScene::TYPE_DIR)
-		{
+		if (type == ScriptSelectScene::TYPE_DIR)
 			dir = systemInfo->GetLastScriptSearchDirectory();
-		}
 		ScriptSelectFileModel* fileModel = new ScriptSelectFileModel(type, dir);
 		std::wstring pathWait = systemInfo->GetLastSelectedScriptPath();
 		fileModel->SetWaitPath(pathWait);
@@ -158,8 +152,7 @@ void SceneManager::TransStgScene(ref_count_ptr<ScriptInformation> infoMain, ref_
 	EDirectInput* input = EDirectInput::GetInstance();
 	input->ClearKeyState();
 
-	try
-	{
+	try {
 		//STGシーン初期化
 		ref_count_ptr<StgSystemInformation> infoStgSystem = new StgSystemInformation();
 		infoStgSystem->SetMainScriptInformation(infoMain);
@@ -177,13 +170,11 @@ void SceneManager::TransStgScene(ref_count_ptr<ScriptInformation> infoMain, ref_
 
 		//STGタスク登録
 		taskManager->AddTask(task);
-		taskManager->AddWorkFunction(TTaskFunction<StgSystemController>::Create(
-			task, &StgSystemController::Work), StgSystemController::TASK_PRI_WORK);
-		taskManager->AddRenderFunction(TTaskFunction<StgSystemController>::Create(
-			task, &StgSystemController::Render), StgSystemController::TASK_PRI_RENDER);
-	}
-	catch(gstd::wexception& e)
-	{
+		taskManager->AddWorkFunction(TTaskFunction<StgSystemController>::Create(task, &StgSystemController::Work),
+			StgSystemController::TASK_PRI_WORK);
+		taskManager->AddRenderFunction(TTaskFunction<StgSystemController>::Create(task, &StgSystemController::Render),
+			StgSystemController::TASK_PRI_RENDER);
+	} catch (gstd::wexception& e) {
 		Logger::WriteTop(e.what());
 
 		SystemController* system = SystemController::GetInstance();
@@ -194,68 +185,59 @@ void SceneManager::TransStgScene(ref_count_ptr<ScriptInformation> infoMain, ref_
 
 void SceneManager::TransStgScene(ref_count_ptr<ScriptInformation> infoMain, ref_count_ptr<ReplayInformation> infoReplay)
 {
-	try
-	{
+	try {
 		std::wstring replayPlayerID = infoReplay->GetPlayerScriptID();
 		std::wstring replayPlayerScriptFileName = infoReplay->GetPlayerScriptFileName();
 
 		//自機を検索
 		ref_count_ptr<ScriptInformation> infoPlayer;
-		std::vector<ref_count_ptr<ScriptInformation> > listPlayer;
+		std::vector<ref_count_ptr<ScriptInformation>> listPlayer;
 		std::vector<std::wstring> listPlayerPath = infoMain->GetPlayerList();
-		if(listPlayerPath.size() == 0)
-		{
-			listPlayer = 
-				SystemController::GetInstance()->GetSystemInformation()->GetFreePlayerScriptInformationList();
-		}
-		else
-		{
+		if (listPlayerPath.size() == 0) {
+			listPlayer = SystemController::GetInstance()->GetSystemInformation()->GetFreePlayerScriptInformationList();
+		} else {
 			listPlayer = infoMain->CreatePlayerScriptInformationList();
 		}
 
-		for(int iPlayer = 0 ; iPlayer < listPlayer.size() ; iPlayer++)
-		{
+		for (int iPlayer = 0; iPlayer < listPlayer.size(); iPlayer++) {
 			ref_count_ptr<ScriptInformation> tInfo = listPlayer[iPlayer];
-			if(tInfo->GetID() != replayPlayerID)continue;
+			if (tInfo->GetID() != replayPlayerID)
+				continue;
 			std::wstring tPlayerScriptFileName = PathProperty::GetFileName(tInfo->GetScriptPath());
-			if(tPlayerScriptFileName != replayPlayerScriptFileName)continue;
+			if (tPlayerScriptFileName != replayPlayerScriptFileName)
+				continue;
 
 			infoPlayer = tInfo;
 			break;
 		}
 
-		if(infoPlayer == NULL)
-		{
+		if (infoPlayer == NULL) {
 			std::wstring log = StringUtility::Format(L"自機スクリプトが見つかりません:[%s]", replayPlayerScriptFileName.c_str());
 			throw gstd::wexception(log.c_str());
 		}
 
 		TransStgScene(infoMain, infoPlayer, infoReplay);
-	}
-	catch(gstd::wexception& e)
-	{
+	} catch (gstd::wexception& e) {
 		Logger::WriteTop(e.what());
 
 		SystemController* system = SystemController::GetInstance();
 		system->ShowErrorDialog(e.what());
 		system->GetSceneManager()->TransScriptSelectScene_Last();
 	}
-
 }
 void SceneManager::TransPackageScene(ref_count_ptr<ScriptInformation> infoMain, bool bOnlyPackage)
 {
 	EDirectInput* input = EDirectInput::GetInstance();
 	input->ClearKeyState();
 
-	try
-	{
+	try {
 		//STGシーン初期化
 		ref_count_ptr<StgSystemInformation> infoStgSystem = new StgSystemInformation();
 		infoStgSystem->SetMainScriptInformation(infoMain);
 		ref_count_ptr<StgSystemController> task = NULL;
-		if(!bOnlyPackage)
+		if (!bOnlyPackage)
 			task = new EStgSystemController();
-		else 
+		else
 			task = new PStgSystemController();
 
 		//STGタスク初期化
@@ -270,26 +252,20 @@ void SceneManager::TransPackageScene(ref_count_ptr<ScriptInformation> infoMain, 
 
 		//STGタスク登録
 		taskManager->AddTask(task);
-		taskManager->AddWorkFunction(TTaskFunction<StgSystemController>::Create(
-			task, &StgSystemController::Work), StgSystemController::TASK_PRI_WORK);
-		taskManager->AddRenderFunction(TTaskFunction<StgSystemController>::Create(
-			task, &StgSystemController::Render), StgSystemController::TASK_PRI_RENDER);
-	}
-	catch(gstd::wexception& e)
-	{
+		taskManager->AddWorkFunction(TTaskFunction<StgSystemController>::Create(task, &StgSystemController::Work),
+			StgSystemController::TASK_PRI_WORK);
+		taskManager->AddRenderFunction(TTaskFunction<StgSystemController>::Create(task, &StgSystemController::Render),
+			StgSystemController::TASK_PRI_RENDER);
+	} catch (gstd::wexception& e) {
 		Logger::WriteTop(e.what());
 
 		SystemController* system = SystemController::GetInstance();
 		system->ShowErrorDialog(e.what());
-		if(!bOnlyPackage)
-		{
+		if (!bOnlyPackage) {
 			system->GetSceneManager()->TransScriptSelectScene_Last();
-		}
-		else
-		{
+		} else {
 			EApplication::GetInstance()->End();
 		}
-
 	}
 }
 
@@ -324,10 +300,10 @@ void TransitionManager::_AddTask(ref_count_ptr<TransitionEffect> effect)
 	ref_count_ptr<SystemTransitionEffectTask> task = new SystemTransitionEffectTask();
 	task->SetTransition(effect);
 	taskManager->AddTask(task);
-	taskManager->AddWorkFunction(TTaskFunction<SystemTransitionEffectTask>::Create(
-		task, &SystemTransitionEffectTask::Work), TASK_PRI);
-	taskManager->AddRenderFunction(TTaskFunction<SystemTransitionEffectTask>::Create(
-		task, &SystemTransitionEffectTask::Render), TASK_PRI);
+	taskManager->AddWorkFunction(TTaskFunction<SystemTransitionEffectTask>::Create(task, &SystemTransitionEffectTask::Work),
+		TASK_PRI);
+	taskManager->AddRenderFunction(TTaskFunction<SystemTransitionEffectTask>::Create(task, &SystemTransitionEffectTask::Render),
+		TASK_PRI);
 }
 void TransitionManager::DoFadeOut()
 {
@@ -347,8 +323,7 @@ void TransitionManager::DoFadeOut()
 void SystemTransitionEffectTask::Work()
 {
 	TransitionEffectTask::Work();
-	if(effect_ != NULL && effect_->IsEnd())
-	{
+	if (effect_ != NULL && effect_->IsEnd()) {
 		WorkRenderTaskManager* taskManager = ETaskManager::GetInstance();
 		taskManager->RemoveTask(this);
 		return;
@@ -371,13 +346,11 @@ SystemInformation::SystemInformation()
 }
 SystemInformation::~SystemInformation()
 {
-
 }
 void SystemInformation::_SearchFreePlayerScript(std::wstring dir)
 {
 	listFreePlayer_ = ScriptInformation::FindPlayerScriptInformationList(dir);
-	for(int iPlayer = 0 ; iPlayer < listFreePlayer_.size() ; iPlayer++)
-	{
+	for (int iPlayer = 0; iPlayer < listFreePlayer_.size(); iPlayer++) {
 		ref_count_ptr<ScriptInformation> info = listFreePlayer_[iPlayer];
 		std::wstring path = info->GetScriptPath();
 		std::wstring log = StringUtility::Format(L"自機スクリプト：%s", path.c_str());
@@ -403,10 +376,10 @@ SystemResidentTask::SystemResidentTask()
 	int screenWidth = graphics->GetScreenWidth();
 	int screenHeight = graphics->GetScreenHeight();
 
-	textFps_.SetFontColorTop(D3DCOLOR_ARGB(255,160,160,255));
-	textFps_.SetFontColorBottom(D3DCOLOR_ARGB(255,64,64,255));
+	textFps_.SetFontColorTop(D3DCOLOR_ARGB(255, 160, 160, 255));
+	textFps_.SetFontColorBottom(D3DCOLOR_ARGB(255, 64, 64, 255));
 	textFps_.SetFontBorderType(directx::DxFont::BORDER_FULL);
-	textFps_.SetFontBorderColor(D3DCOLOR_ARGB(255,255,255,255));
+	textFps_.SetFontBorderColor(D3DCOLOR_ARGB(255, 255, 255, 255));
 	textFps_.SetFontBorderWidth(2);
 	textFps_.SetFontSize(14);
 	textFps_.SetFontBold(true);
@@ -420,8 +393,10 @@ SystemResidentTask::~SystemResidentTask()
 void SystemResidentTask::RenderFps()
 {
 	WorkRenderTaskManager* taskManager = ETaskManager::GetInstance();
-	if(taskManager->GetTask(typeid(EStgSystemController)) != NULL)return;
-	if(taskManager->GetTask(typeid(PStgSystemController)) != NULL)return;
+	if (taskManager->GetTask(typeid(EStgSystemController)) != NULL)
+		return;
+	if (taskManager->GetTask(typeid(PStgSystemController)) != NULL)
+		return;
 
 	DirectGraphics* graphics = DirectGraphics::GetBase();
 	graphics->SetBlendMode(DirectGraphics::MODE_BLEND_ALPHA);
