059cdf0b7ba87975aa99242833e1d4ead7246644
CreepyNinja_ <creepyninja.yt@gmail.com>
2019-07-28 15:16:11 +0200

    clang-format: lint GcLibImpl

--- source/TouhouDanmakufu/DnhConfig/GcLibImpl.hpp
+++ source/TouhouDanmakufu/DnhConfig/GcLibImpl.hpp
@@ -1,24 +1,25 @@
 #ifndef __TOUHOUDANMAKUFU_CONFIG_GCLIBIMPL__
 #define __TOUHOUDANMAKUFU_CONFIG_GCLIBIMPL__
 
-#include"Constant.hpp"
-#include"../Common/DnhGcLibImpl.hpp"
+#include "../Common/DnhGcLibImpl.hpp"
+#include "Constant.hpp"
 
 /**********************************************************
 //EApplication
 **********************************************************/
-class EApplication : public Singleton<EApplication>, public Application
-{
-		friend Singleton<EApplication>;
-		EApplication();
-	protected:
-		bool _Initialize();
-		bool _Loop();
-		bool _Finalize();
-	public:
-		~EApplication();
+class EApplication : public Singleton<EApplication>, public Application {
+	friend Singleton<EApplication>;
+
+private:
+	EApplication();
+
+protected:
+	bool _Initialize();
+	bool _Loop();
+	bool _Finalize();
+
+public:
+	~EApplication();
 };
-
-
 
 #endif
