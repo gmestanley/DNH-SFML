73067da470ac9d3d5119869c5e1b9466f8ea2ebc
gmestanley <agsproductionsnew@outlook.com>
2020-11-09 23:50:10 -0300

    Add files via upload

---
+++ source/TouhouDanmakufu/Common/Networking.hpp
@@ -0,0 +1,15 @@
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "SFML/Network.hpp"
+#include <iostream>
+
+class Netplay {
+public:
+	static sf::TcpSocket t_socket;
+	static sf::UdpSocket u_socket;
+	static char in[128];
+	static char* convertToChar(wchar_t a, int size);
+	static std::wstring convertToWString(const std::string &s);
+};
